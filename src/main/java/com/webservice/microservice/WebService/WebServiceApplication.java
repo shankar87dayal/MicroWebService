feefcaa220fe3ee7195cfe991ad209852bfe1739
shankar <shankar.87.dayal@gmail.com>
2019-04-06 14:02:00 +0530

    Initial commit

---
+++ src/main/java/com/webservice/microservice/WebService/WebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.webservice.microservice.WebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceApplication.class, args);
+	}
+
+}
